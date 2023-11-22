ae1bbd551603b863a1ad3fd0497ae14b7fb7cb94
MarkMusili <musiliyrn@gmail.com>
2023-11-23 00:46:28 +0300

    Changes

--- models/amenity.py
+++ models/amenity.py
@@ -11,5 +11,3 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    places = relationship('Place', secondary='place_amenity', viewonly=False,
-                          back_populates='amenities')
