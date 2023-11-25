70b96c124179049c8326f125c9825d9096f289cf
Ote Ian <oteian9@gmail.com>
2023-11-25 18:26:10 +0000

    Pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -8,58 +8,60 @@ from datetime import datetime
 
 
 Base = declarative_base()
+
+
 class BaseModel:
-	"""A base class for all hbnb models"""
-	id = Column(String(60), nullable=False, primary_key=True)
-	created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-	updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-	def __init__(self, *args, **kwargs):
+    """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-		format = '%Y-%m-%dT%H:%M:%S.%f'
-		if kwargs:
-			for key, value in kwargs.items():
-				if key != '__class__':
-					if key == 'created_at' or key == 'updated_at':
-						setattr(self, key, datetime.strptime(value, format))
-					else:
-						setattr(self, key, value)
-			if 'id' not in kwargs:
-				self.id = str(uuid.uuid4())
-			if 'created_at' not in kwargs:
-				self.created_at = datetime.now()
-				self.updated_at = self.created_at
-		else:
-			self.id = str(uuid.uuid4())
-			self.created_at = datetime.now()
-			self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
 
+        format = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, format))
+                    else:
+                        setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+                self.updated_at = self.created_at
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
-	def __str__(self):
-		"""Returns a string representation of the instance"""
-		cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-		return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
-	def save(self):
-		"""Updates updated_at with current time when instance is changed"""
-		from models import storage
-		self.updated_at = datetime.now()
-		storage.new(self)
-		models.storage.new(self)
-		storage.save()
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.new(self)
+        models.storage.new(self)
+        storage.save()
 
-	def to_dict(self):
-		"""Convert instance into dict format"""
-		dictionary = {}
-		dictionary.update(self.__dict__)
-		dictionary.update({'__class__':
-								(str(type(self)).split('.')[-1]).split('\'')[0]})
-		dictionary['created_at'] = self.created_at.isoformat()
-		dictionary['updated_at'] = self.updated_at.isoformat()
-		if ('_sa_instance_state') in dictionary.keys():
-			del dictionary['_sa_instance_state']
-		return dictionary
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        if ('_sa_instance_state') in dictionary.keys():
+            del dictionary['_sa_instance_state']
+        return dictionary
 
-	def delete(self):
-			"""Deletes current instance from storage"""
-			from models import storage
-			storage.delete(self)
+    def delete(self):
+        """Deletes current instance from storage"""
+        from models import storage
+        storage.delete(self)
