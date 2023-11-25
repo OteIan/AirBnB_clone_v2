857592572948554b29ca54188ee3c4cb11d1c524
Ote Ian <oteian9@gmail.com>
2023-11-25 23:00:15 +0300

    More fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,47 +13,48 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 class DBStorage:
-"""Database storage"""
+    """Database storage"""
     __engine = None
     __session = None
 
     def __init__(self):
-    self.__engine = create_engine(
-                                'mysql+mysqldb://{}:{}@{}:3306/{}'.
-                                format(
-                                        os.getenv('HBNB_MYSQL_USER'),
-                                        os.getenv('HBNB_MYSQL_PWD'),
-                                        os.getenv('HBNB_MYSQL_HOST'),
-                                        os.getenv('HBNB_MYSQL_DB'),
-                                        pool_pre_ping=True
-                                        )
-                                )
-    if os.getenv('HBNB_ENV') == 'test':
-        Base.metadata.drop_all(self.__engine)
+        """ """
+        self.__engine = create_engine(
+                                    'mysql+mysqldb://{}:{}@{}:3306/{}'.
+                                    format(
+                                            os.getenv('HBNB_MYSQL_USER'),
+                                            os.getenv('HBNB_MYSQL_PWD'),
+                                            os.getenv('HBNB_MYSQL_HOST'),
+                                            os.getenv('HBNB_MYSQL_DB'),
+                                            pool_pre_ping=True
+                                            )
+                                    )
+        if os.getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
 
-        self.__session = scoped_session(sessionmaker(bind=self.__engine,
-            expire_on_commit=False))
+            self.__session = scoped_session(sessionmaker(bind=self.__engine,
+                expire_on_commit=False))
 
-        def all(self, cls=None):
-            """"Query on the current database session all 
-        objects depending on class name"""
-        objects = {}
-        if cls:
-            if type(cls) == str:
-                cls = eval(cls)
-            query = self.__session.query(cls)
-            for obj in query:
-                key = "{}.{}".format(type(obj).__name__, obj.id)
-                objects[key] = obj
-        else:
-            classes = [User, State, City, Amenity, Place, Review]
-            for element in classes:
-                query = self.__session.query(element)
-                for obj in query:
-                    key = "{}.{}".format(type(obj).__name__, obj.id)
-                    objects[key] = obj
+            def all(self, cls=None):
+                """"Query on the current database session all 
+                objects depending on class name"""
+                objects = {}
+                if cls:
+                    if type(cls) == str:
+                        cls = eval(cls)
+                    query = self.__session.query(cls)
+                    for obj in query:
+                        key = "{}.{}".format(type(obj).__name__, obj.id)
+                        objects[key] = obj
+                else:
+                    classes = [User, State, City, Amenity, Place, Review]
+                    for element in classes:
+                        query = self.__session.query(element)
+                        for obj in query:
+                            key = "{}.{}".format(type(obj).__name__, obj.id)
+                            objects[key] = obj
 
-        return objects
+                return objects
 
     def new(self, obj):
         """Creates a new object"""
@@ -65,7 +66,7 @@ class DBStorage:
 
     def delete(self, obj=None):
         """Deletes an object"""
-        if obj != None:
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
