9d14e9d14f012e80c2b3513a4847286afe977d79
Ote Ian <oteian9@gmail.com>
2023-11-25 18:26:45 +0000

    Pycodestyle

--- models/state.py
+++ models/state.py
@@ -5,18 +5,20 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.city import City
 
-class State(BaseModel, Base):
-	""" State class """
-	__tablename__ = "states"
-	name = Column(String(128), nullable=False)
-	cities = relationship('City', cascade='all, delete-orphan', back_populates="state")
 
-	@property
-	def cities(self):
-		"""Returns a list of City instances"""
-		from models.__init__ import storage
-		cities = []
-		for obj in storage.all(City).values():
-			if obj.state_id == self.id:
-				cities.append(obj)
-			return cities
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete-orphan',
+                          back_populates="state")
+
+    @property
+    def cities(self):
+        """Returns a list of City instances"""
+        from models.__init__ import storage
+        cities = []
+        for obj in storage.all(City).values():
+            if obj.state_id == self.id:
+                cities.append(obj)
+            return cities
