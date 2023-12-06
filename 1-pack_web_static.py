c582cb97778540d06b7934b9103a147da4087e20
MarkMusili <musiliyrn@gmail.com>
2023-12-06 13:38:37 +0300

    Package our files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from fabric.api import local
+from datetime import datetime
+from os import path, makedirs
+"""Fabric script that generates a .tgz archive
+from the contents of the web_static folder"""
+
+
+@task
+def do_pack():
+	"""Package the files in form of .tgz"""
+	try:
+		if not path.exists("versions"):
+			makedirs("versions")
+
+		now = datetime.now()
+		archive_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year,
+															now.month,
+															now.day,
+															now.hour,
+															now.minute,
+															now.second)
+		
+		# Compress the files from web_static
+		local(f"tar -cvzf versions/{archive_name} web_static")
+
+		return f"versions/{archive_name}"
+	except Exception as e:
+		print(e)
+		return None
