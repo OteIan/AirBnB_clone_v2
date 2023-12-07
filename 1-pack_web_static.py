96c426f7e7b9c81143e85a04a7debcc078ea369c
MarkMusili <musiliyrn@gmail.com>
2023-12-07 12:45:53 +0000

    Pycodestyle fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,29 +7,30 @@ from the contents of the web_static folder"""
 
 
 def do_pack():
-	"""Package the files in form of .tgz"""
-	try:
-		if not path.exists("versions"):
-			makedirs("versions")
+    """Package the files in form of .tgz"""
+    try:
+        if not path.exists("versions"):
+            makedirs("versions")
 
-		now = datetime.now()
-		archive_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year,
-                        now.month,
-                        now.day,
-                        now.hour,
-                        now.minute,
-                        now.second)
-		
-		# Compress the files from web_static
-		local(f"tar -cvzf versions/{archive_name} web_static")
+        now = datetime.now()
+        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year,
+                                                            now.month,
+                                                            now.day,
+                                                            now.hour,
+                                                            now.minute,
+                                                            now.second)
+        # Compress the files from web_static
+        local(f"tar -cvzf versions/{archive_name} web_static")
 
-		# Get the size of the created archive
-		archive_size = path.getsize("versions/{}".format(archive_name))
+        # Get the size of the created archive
+        archive_size = path.getsize("versions/{}".format(archive_name))
 
         # Print a message with the packed file and its size
-		print("web_static packed: versions/{} -> {}Bytes".format(archive_name, archive_size))
+        a = archive_name
+        b = archive_size
+        print(f"web_static packed: versions/{a} -> {b}Bytes")
 
-		return "versions/{}".format(archive_name)
-	except Exception as e:
-		print(e)
-		return None
+        return "versions/{}".format(archive_name)
+    except Exception as e:
+        print(e)
+        return None
