92b71c4903e6b9e7c0b4a70d6de8f2faac3d2b34
Ote Ian <oteian9@gmail.com>
2023-12-31 21:20:58 +0300

    Fix: pycodestyle

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship('City', cascade='all, delete-orphan',
-                            backref="state")
+                              backref="state")
 
 else:
     class State(BaseModel):
