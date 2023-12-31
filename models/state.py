3c4b3e0e25ca0c57c26eb682e11ed095ff194a08
Ote Ian <oteian9@gmail.com>
2023-12-31 21:14:04 +0300

    Fix: getter method if storage method isnt DBStorage

--- models/state.py
+++ models/state.py
@@ -6,20 +6,23 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.city import City
 
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete-orphan',
+                            backref="state")
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete-orphan',
-                          backref="state")
-
-    @property
-    def cities(self):
-        """Returns a list of City instances"""
-        from models.__init__ import storage
-        cities = []
-        for obj in storage.all(City).values():
-            if obj.state_id == self.id:
-                cities.append(obj)
-        return cities
+else:
+    class State(BaseModel):
+        """ State class """
+        @property
+        def cities(self):
+            """Returns a list of City instances"""
+            from models.__init__ import storage
+            cities = []
+            for obj in storage.all(City).values():
+                if obj.state_id == self.id:
+                    cities.append(obj)
+            return cities
